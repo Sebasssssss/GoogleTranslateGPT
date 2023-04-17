5cd6260ccc0fa464a0637b67822ce3c3a7d60ed1
Sebasssssss <sebastian.rodriguez9714@gmail.com>
2023-04-17 19:38:30 -0300

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Translate</h1>
+    </div>
+  )
+}
+
+export default App
