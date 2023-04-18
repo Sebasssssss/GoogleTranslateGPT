f14d09079ca387a847deca1625e729e9ba3af6ee
Sebasssssss <sebastian.rodriguez9714@gmail.com>
2023-04-18 19:06:21 -0300

    Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useStore } from './hooks/useStore'
 
 function App() {
+  const { fromLanguage, setFromLanguage } = useStore()
   return (
     <div className="App">
       <h1>Google Translate</h1>
+      <button
+        onClick={() => {
+          setFromLanguage('es')
+        }}
+      >
+        Change language
+      </button>
+      <h1>{fromLanguage}</h1>
     </div>
   )
 }
---
+++ src/hooks/useStore.ts
@@ -0,0 +1,77 @@
+import { useReducer } from 'react'
+import {
+  type FromLanguage,
+  type Language,
+  type Action,
+  type State
+} from '../types.d'
+
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+const reducer = (state: State, action: Action) => {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return { ...state, fromLanguage: action.payload }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return { ...state, toLanguage: action.payload }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return { ...state, loading: true, fromText: action.payload, result: '' }
+  }
+
+  if (type === 'SET_RESULT') {
+    return { ...state, loading: false, result: action.payload }
+  }
+
+  return state
+}
+
+export function useStore() {
+  const [{ fromLanguage, toLanguage, fromText, result, loading }, dispatch] =
+    useReducer(reducer, initialState)
+
+  const interchangeLanguages = () => dispatch({ type: 'INTERCHANGE_LANGUAGES' })
+
+  const setFromLanguage = (payload: FromLanguage) =>
+    dispatch({ type: 'SET_FROM_LANGUAGE', payload })
+
+  const setToLanguage = (payload: Language) =>
+    dispatch({ type: 'SET_TO_LANGUAGE', payload })
+
+  const setFromText = (payload: string) =>
+    dispatch({ type: 'SET_FROM_TEXT', payload })
+
+  const setResult = (payload: string) =>
+    dispatch({ type: 'SET_RESULT', payload })
+
+  return {
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult
+  }
+}
